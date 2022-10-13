1b30cfc5087a252396e3f1f39bc4f950f5eea4bd
dumoresco <eduardomorescoiost@gmail.com>
2022-10-12 22:53:48 -0300

    fix images path

--- src/components/UI/JobCard/index.tsx
+++ src/components/UI/JobCard/index.tsx
@@ -23,7 +23,7 @@ const JobCard: React.FC<JobCardInterface> = ({
   return (
     <Container onClick={() => showJobDetails(id)}>
       <Logo className="img" logoBackground={logoBackground}>
-        <img src={`src/${logo}`} alt="" />
+        <img src={`../public/${logo}`} alt="" />
       </Logo>
       <div className="main-infos">
         <p>
